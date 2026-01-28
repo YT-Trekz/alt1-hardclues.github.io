50a798e3a6e253da1d5f333a51477b2851fc127a
YT-Trekz <95730809+YT-Trekz@users.noreply.github.com>
2026-01-29 00:21:59 +0100

    Add files via upload

---
+++ data/clues.js
@@ -0,0 +1,36 @@
+export const hardClues = [
+    {
+        id: "coordinate_ardougne_river",
+        type: "coordinate",
+        keywords: [
+            "clue",
+            "04 degrees",
+            "03 minutes",
+            "south",
+            "03 degrees",
+            "11 minutes",
+            "east"
+        ],
+        images: [
+            "data/hardclues/clue0.png",
+            "data/hardclues/clue0_step1.png",
+            "data/hardclues/clue0_step2.png"
+        ]
+    },
+    {
+        id: "coordinate_tyras_camp",
+        type: "coordinate",
+        keywords: [
+            "clue",
+            "00 degrees",
+            "00 minutes",
+            "north",
+            "07 degrees",
+            "13 minutes",
+            "west"
+        ],
+        images: [
+            "data/hardclues/clue1_step.png"
+        ]
+    }
+];
---
+++ data/locations.js
@@ -0,0 +1,9 @@
+export const coordinateLocations = [
+    {
+        south: "04° 03'",
+        east: "03° 11'",
+        name: "South of Ardougne – by the river",
+        hint: "Dig at the river bend"
+    }
+];
+
